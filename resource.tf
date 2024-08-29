395e37662b2c20f229620df8ab25e63ecb34884c
Rohit Rewapati <147910740+rohitrewapati@users.noreply.github.com>
2024-08-29 13:39:55 +0530

    Update resource.tf
    new update on resource file

--- resource.tf
+++ resource.tf
@@ -1,17 +1,18 @@
 data "aws_availability_zones" "available" {}
 
 resource "aws_instance" "my_instances" {
-  count = 3
+  count = 2
 
-  ami           = "ami-0224820b4e90b6949"
-  instance_type = "t2.micro"
-  key_name      = "shweta-terraform-key"
-  vpc_security_group_ids = ["sg-065d6e671aceef7be"]
-  availability_zone = data.aws_availability_zones.available.names[count.index]
-  subnet_id = "subnet-08a89211d52d8b7f7"
+  ami           = "ami-0224820b4e90b6949" 
+  instance_type = "t2.micro" 
+  availability_zone = element(["us-east-1a","us-east-1b"], count.index)
+  key_name      = "shweta-terraform-key" 
+  subnet_id = element(["subnet-09c70bcefed00afad","subnet-08a89211d52d8b7f7"], count.index)
 
-    tags = {
-    Name = "Rohit-Terraform-${count.index}"
+  tags = {
+    Name = "rohitt-terraf-${count.index}"
   }
+
+  security_groups = ["sg-065d6e671aceef7be"] 
   
 }
